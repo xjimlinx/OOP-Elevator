cbbc0e3ed42eed65cec6ad42660857f4aa62f9ae
xjimlinx <1021538027@qq.com>
2023-04-21 02:19:46 +0800

    用Cmake实现

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xein/Documents/CPPTEST/Elevator
-# Build directory: /home/xein/Documents/CPPTEST/Elevator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
