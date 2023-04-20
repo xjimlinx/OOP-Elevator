5787a4fbdf9df3848d57ad8e6ff9db0a3bb76d28
xjimlinx <1021538027@qq.com>
2023-04-21 02:18:01 +0800

    用Cmake实现

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xein/Documents/CPPTEST/Elevator/src/elevator.cpp" "CMakeFiles/main.dir/src/elevator.cpp.o" "gcc" "CMakeFiles/main.dir/src/elevator.cpp.o.d"
+  "/home/xein/Documents/CPPTEST/Elevator/src/elevsystem.cpp" "CMakeFiles/main.dir/src/elevsystem.cpp.o" "gcc" "CMakeFiles/main.dir/src/elevsystem.cpp.o.d"
+  "/home/xein/Documents/CPPTEST/Elevator/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/xein/Documents/CPPTEST/Elevator/src/person.cpp" "CMakeFiles/main.dir/src/person.cpp.o" "gcc" "CMakeFiles/main.dir/src/person.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xein/Documents/CPPTEST/Elevator
+# Build directory: /home/xein/Documents/CPPTEST/Elevator/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
