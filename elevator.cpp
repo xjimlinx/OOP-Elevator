d00a0e3b3f29f5f14292d1d4b023c22515b3e61e
xjimlinx <1021538027@qq.com>
2023-04-20 21:53:06 +0800

    problem description add

---
+++ elevator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Elevator
+{
+    int currentFloor = 0;
+    int maxPeople = 15;
+    int destinationFloor[40];
+
+    int numberOfFloors = 40;
+
+    int numberOfPeople;
+
+    int doorStatus;
+
+};
+
